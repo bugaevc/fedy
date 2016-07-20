a71eae651b97592b2b79a1121e6669db15000a04
Sergey Bugaev <bugaevc@gmail.com>
2016-07-20 19:18:29 +0300

    Update VirtualBox to 5.1

--- plugins/oraclevirtualbox.plugin/install.sh
+++ plugins/oraclevirtualbox.plugin/install.sh
@@ -4,4 +4,4 @@ dnf config-manager --add-repo=http://download.virtualbox.org/virtualbox/rpm/fedo
 
 dnf -y install binutils gcc make patch libgomp glibc-headers glibc-devel kernel-headers kernel-devel dkms
 
-dnf -y install VirtualBox-5.0
+dnf -y install VirtualBox-5.1
